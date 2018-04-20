ec2989349556d34482ef2a7a90c66acef9dfa5e8
rosema2012 <36199346+rosema2012@users.noreply.github.com>
2018-04-20 13:52:23 +0200

    Add files via upload

--- ReEjercicio1.java
+++ ReEjercicio1.java
@@ -1,53 +1,109 @@
 package reejercicio1;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.io.FilenameFilter;
 import java.io.IOException;
+import java.util.Scanner;
 
 /**
  * @author David Carrera Otero
  */
 public class ReEjercicio1 {
 
+    /**
+     * @author Rosema
+     */
+    public void DividirArchivoEnVarios(File f, int num, boolean opcion) throws IOException {
+        FileReader lectorArchivo = null;
+        FileWriter escritorArchivo = null;
+        Scanner sc = null;
+        String texto = "";
+        char buffer[] = new char[num]; //cargo un grupo de caracteres en el buffer
+        try {
+            sc = new Scanner(f);
+            lectorArchivo = new FileReader(f);
+            int numArchivo = 2;
+            int i;
+            if (opcion) {
+                while ((i = lectorArchivo.read(buffer)) != -1) {
+                    escritorArchivo = new FileWriter("Archivo" + numArchivo + ".txt");
+                    texto = (new String(buffer, 0, i)); // guardamos caracteres en el buffer
+                            escritorArchivo.write(texto);
 
-    
-    public void DividirArchivo(File f,int numLetras) throws IOException{
-        FileReader fileIn = null;
-        FileWriter fileOut = null;
-        String texto;
-        int num = 2;
-        int i;
-        try{
-            fileIn = new FileReader(f);
-            char buffer [] = new char [numLetras];
-            while ((i = fileIn.read(buffer)) != -1) {
-                fileOut = new FileWriter("Archivo"+num+".txt");
-                texto = (new String(buffer,0,i));
-                //System.out.print(texto);
-                fileOut.write(texto);
-                fileOut.close();
-                num++;
+                    escritorArchivo.close();
+                    numArchivo++;
+                }
+            } else {
+                while (sc.hasNextLine()) {
+                    escritorArchivo = new FileWriter("Archivo" + numArchivo + ".txt");
+                    for (int k = 0; k < num; k++) {
+                        if (sc.hasNextLine()) {
+                            escritorArchivo.write(sc.nextLine() + "\n");
+                        } else {
+
+                        }
+
+                    }
+                    numArchivo++;
+                    escritorArchivo.close();
+                }
             }
-            
-            
-        }finally{
-            if(fileOut != null){
-                fileOut.close();
+            num = 2;
+        } finally {
+            if (lectorArchivo != null) {
+                lectorArchivo.close();
             }
-            if(fileIn != null){
-                fileIn.close();
+            if (escritorArchivo != null) {
+                escritorArchivo.close();
+            }
+            if (sc != null) {
+                sc.close();
             }
         }
     }
 
-    public static void main(String[] args) throws FileNotFoundException, IOException {
+    public void leerArchivo(File f, File Directorio) throws FileNotFoundException, IOException {
+
+        FileReader fileLectura = null;
+        FileWriter fileEscritura = null;
+        try {
+            int i;
+
+            String cadena = "";
+            fileEscritura = new FileWriter("ArchivoComprimido.txt");
+            fileLectura = new FileReader(f);
+            for (File archivo : Directorio.listFiles()) {
+                if (archivo.isDirectory()) {
+                } 
+                else if(archivo.isFile() && archivo.getName().endsWith(".txt")){
+                    fileLectura = new FileReader(archivo);
+                    while((i = fileLectura.read())!= -1){
+                        cadena += (char)i;
+                    }
+                }
+            }
+            fileEscritura.write(cadena);
+            fileEscritura.close();
+            fileLectura.close();
+
+        } finally {
+            if (fileLectura != null) {
+                fileLectura.close();
+            }
+            if (fileEscritura != null) {
+                fileEscritura.close();
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
         ReEjercicio1 eje1 = new ReEjercicio1();
-        File f = new File("C:\\Users\\David Carrera Otero\\Documents\\NetBeansProjects\\ReEjercicio1\\src\\reejercicio1\\Archivo.txt");
-        eje1.DividirArchivo(f,60);
-        
+        File f = new File("Archivo.txt");
+        File Directorio = new File("C:\\Users\\David Carrera Otero\\Documents\\NetBeansProjects\\ReEjercicio1");
+        //eje1.DividirArchivoEnVarios(f, 4, false);
+        eje1.leerArchivo(f, Directorio);
     }
 }
